100cba0e7b7d0f50fed0583443083d3d25acee20
Joao Pedro <joaopedrombvboas@gmail.com>
2019-04-21 20:59:09 -0300

    Remove Trash from Dist

--- server/dist/helpers/ResponseHelper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const helper = (message, statusCode) => {
-    return {
-        "message": message,
-        "status_code": statusCode,
-    };
-};
-exports.default = helper;
